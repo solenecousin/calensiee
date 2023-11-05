bdaef64470ac7edd0503ea8501f4f6ff3d109da8
Sol <solenecousin3@gmail.com>
2023-11-05 17:16:53 +0100

    Ajout des classes de la mise en place du calendrier et des layouts pour l'évènement du calendrier et changement des png pour l'icone

--- app/src/main/java/com/example/calensiee/MainActivity.java
+++ app/src/main/java/com/example/calensiee/MainActivity.java
@@ -1,94 +1,81 @@
 package com.example.calensiee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.fragment.app.Fragment;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements BottomMenuFragment.OnItemClickListener,MoisFragment.OnItemClickListener {
-    //Button boutonMois;
+import static com.example.calensiee.CalendarUtils.daysInMonthArray;
+import static com.example.calensiee.CalendarUtils.monthYearFromDate;
 
-
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomMenuFragment bottomMenuFragment = new BottomMenuFragment();
-        SemaineFragment semaineFragment = new SemaineFragment();
-
-
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_fragment_layout, semaineFragment)
-                .add(R.id.menu_fragment_layout, bottomMenuFragment)
-                .commit();
-
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
     }
 
-    //@Override // for nested class ListFragment.onItemClickListener
-    public void onItemSelectedBottom(int position) {
-        // Create a Toast that displays the position that was clicked
-        Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
 
-        // Replace the current fragment with a new one
-        BottomMenuFragment bottomMenuFragment = new BottomMenuFragment();
-        SemaineFragment semaineFragment = new SemaineFragment();
-        MoisFragment moisFragment = new MoisFragment();
-        ReglagesFragment reglagesFragment = new ReglagesFragment();
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
 
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
 
-        if(position==1){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_fragment_layout, semaineFragment)
-                    //.add(R.id.main_fragment_layout, bottomMenuFragment)
-                    .commit();
-        }
-        else if(position==2){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_fragment_layout, moisFragment)
-                    //.add(R.id.main_fragment_layout, bottomMenuFragment)
-                    .commit();
-        }
-        else if(position==3){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_fragment_layout, reglagesFragment)
-                    //.add(R.id.main_fragment_layout, bottomMenuFragment)
-                    .commit();
+    public void previousMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+        if(date != null)
+        {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
         }
     }
 
-    public void onItemSelectedJour(int position, ArrayList<Integer> dayEvent) {
-        Bundle b = new Bundle();
-        int taille=dayEvent.size();
+    public void weeklyAction(View view)
+    {
 
-        //int dayEvent=mois.getListeEvents().get(0);
-        // Create a Toast that displays the position that was clicked
-        Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
-
-        // Replace the current fragment with a new one
-        BottomMenuFragment bottomMenuFragment = new BottomMenuFragment();
-        EvenementFragment evenementFragment = new EvenementFragment();
-
-        for(int k=0; k<taille; k++){
-            if(position== dayEvent.get(k)){
-                b.putString("dayEvent", String.valueOf(position));
-                b.putString("monthEvent", String.valueOf(11));
-                evenementFragment.setArguments(b);
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.main_fragment_layout, evenementFragment)
-                        .commit();
-            }
-        }
-
+        startActivity(new Intent(this, WeekViewActivity.class));
     }
 }
-
