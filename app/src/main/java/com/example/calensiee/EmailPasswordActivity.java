4745af9234223e0addf84e98ea30189a06b8b7e5
Sol <solenecousin3@gmail.com>
2023-11-18 14:35:39 +0100

    suppression de la méthode create account dans EmailPasswordActivity car déjà présente dans accountcreate

--- app/src/main/java/com/example/calensiee/EmailPasswordActivity.java
+++ app/src/main/java/com/example/calensiee/EmailPasswordActivity.java
@@ -72,30 +72,7 @@ public class EmailPasswordActivity extends AppCompatActivity
     }
     // [END on_start_check_user]
 
-    private void createAccount(String email, String password) {
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                            Intent intent = new Intent(EmailPasswordActivity.this, MenuView.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-        // [END create_user_with_email]
-    }
+
 
     private void signIn(String email, String password) {
         // [START sign_in_with_email]
